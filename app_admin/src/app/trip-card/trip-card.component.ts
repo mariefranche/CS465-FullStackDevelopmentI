7147d41dad282a47aa3daa3220ddcbe641496477
Michelle Lewis <michelle.lewis10@snhu.edu>
2024-08-14 00:17:40 -0400

    Module 6 completed baseline

--- app_admin/src/app/trip-card/trip-card.component.ts
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { Trip } from '../models/trip';
 
 @Component({
   selector: 'app-trip-card',
@@ -12,9 +14,15 @@ export class TripCardComponent implements OnInit {
   
   @Input('trip') trip: any;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    // Initialization logic, if needed
+  }
 
+  public editTrip(trip: Trip) {
+    localStorage.removeItem('tripCode');
+    localStorage.setItem('tripCode', trip.code);
+    this.router.navigate(['edit-trip']);
   }
 }
