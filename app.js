87369a61fa44767bbbe5a6bf49c598477d6943c3
Michelle Lewis <michelle.lewis10@snhu.edu>
2024-08-17 00:10:05 -0400

    Module 7 completed baseline

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -15,38 +17,61 @@ var handlebars = require('hbs'); // define handlebars variable
 // Bring in the database
 require('./app_api/models/db'); // Connect to DB
 
+require('./app_api/models/user'); // Not in directions but need to load model user file
+
+// Wire in our authentication module
+var passport = require('passport');
+require('./app_api/config/passport');
+
 var app = express();
 
-// view engine setup
+// Set up middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Initialize Passport middleware
+app.use(passport.initialize());
+
+
+// View engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
 
-// register handlebars partials (https://www.npmjs.com/package/hbs)
+// Register handlebars partials (https://www.npmjs.com/package/hbs)
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Enable CORS
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
 
-// wire-up routes to controllers
+// Wire-up routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/api', apiRouter);  // wire-up API routes
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+});
+
+// Error handling
+// Catch unauthorized error and create 401
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res
+      .status(401)
+      .json({ "message": err.name + ": " + err.message });
+  }
 });
 
 // error handler
