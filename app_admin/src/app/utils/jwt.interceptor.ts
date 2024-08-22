91602c850230490f0890e3ac6fa99f5909c7be87
Michelle Lewis <michelle.lewis10@snhu.edu>
2024-08-22 14:36:31 -0400

    Module 7 completed baseline

--- app_admin/src/app/utils/jwt.interceptor.ts
+++ app_admin/src/app/utils/jwt.interceptor.ts
@@ -4,36 +4,38 @@ import { HttpInterceptor, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { AuthenticationService } from "../services/authentication.service";
 
+
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+  
   constructor(
     private authenticationService: AuthenticationService
   ) {}
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> { 
-    //console.log('Intercepting request:', request.url); // CONSOLE CUSTOM
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     var isAuthAPI: boolean;
 
-    //console.log('Interceptor::URL' + request.url); // CONSOLE
-    if (request.url.startsWith('login') || request.url.startsWith('register')) { 
-      isAuthAPI = true; 
-    } else { 
-      isAuthAPI = false; 
-    } 
+    console.log('Interceptor::URL ' + request.url); // CONSOLE
+    if (request.url.startsWith('login') || request.url.startsWith('register')) {
+      isAuthAPI = true;
+    } else {
+      isAuthAPI = false;
+    }
 
-    if (this.authenticationService.isLoggedIn() && !isAuthAPI) { 
+    if (this.authenticationService.isLoggedIn() && !isAuthAPI) {
       let token = this.authenticationService.getToken();
-      //console.log(token);  // CONSOLE
-      const authReq = request.clone({ 
-        setHeaders: { 
-          Authorization: `Bearer ${token}` 
-        } 
-      }); 
-      return next.handle(authReq); 
-    } 
-    
-    return next.handle(request); 
-  } 
+      console.log('Token:', token);  // CONSOLE
+      const authReq = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      console.log('Modified Request Headers:', authReq.headers); 
+      return next.handle(authReq);
+    }
+
+    return next.handle(request);
+  }
 }
 
 export const authInterceptProvider: Provider = {
